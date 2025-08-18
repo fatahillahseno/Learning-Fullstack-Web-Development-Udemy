05162831f4533ec2479d98ce61ea8624dcf0cf98
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-08-18 10:53:44 +0700

    Terdapat cara alternatif ketika mendeklarasikan sebuah array, yang menggunakan constructor function:
    new Array();
    
    ada cara menambahkan dan menghapus value pada array, namun akan menambahkan atau menghapus di index paling terakhir pada array.
    
    menggunakan function
    .push -> menambahkan value
    dan
    .pop -> menghapus value
    
    Menyalin array agar tidak menggunakan memori yang sama yaitu dengan spread operator
    
    [  ..contohArray]

--- 69 - html-css-js-13/script.js
+++ 69 - html-css-js-13/script.js
@@ -1,8 +1,13 @@
 let fruits = ["Apple", "Banana", "Orange", "Mango"];
-console.log(fruits);
+console.log(fruits); /* extract seluruh data pada array */
+console.log(fruits[0]); /* extract data indeks ke 0 pada array */
+console.log(fruits[1]); /* extract data indeks ke 1 pada array */
+console.log(fruits[2]); /* extract data indeks ke 2 pada array */
+console.log(fruits[3]); /* extract data indeks ke 3 pada array */
 
 let numbers = [10, 20, 30, 40, 50];
-console.log(numbers);
+console.log("\n", numbers);
+console.log(numbers[1]); /* extract data indeks ke 1 pada array */
 
 let mixedArray = [
   "Hello",
@@ -12,5 +17,43 @@ let mixedArray = [
   { name: "Seno", job: "Software Engineer" },
   [13, 7, 8],
 ];
+console.log("\n", mixedArray);
+console.log(
+  mixedArray[4]
+); /* extract data nested object, indeks ke 4 pada array */
+console.log(
+  mixedArray[4]["name"]
+); /* extract data name pada nested object menggunkan square bracket*/
+console.log(
+  mixedArray[4].name
+); /* extract data name pada nested object menggunakan dot*/
+console.log(mixedArray[5]);
+console.log(mixedArray[5][1]);
 
-console.log(mixedArray);
+// Cara Alternatif Membuat Array
+// Constructor Function
+let score = new Array(75, 89, 64, 30);
+// score[0] = 75;
+// score[1] = 89;
+// score[2] = 64;
+// score[3] = 30;
+console.log(score);
+
+// menambahkan value pada array
+// variabelArray.push
+score.push(88);
+console.log(score);
+
+// array juga termasuk tipe data yang mutable
+// let newFruits = fruits;
+/* cara yang salah untuk menyalin array karena masih mengambil memori yang sama pada array yang direferensikan */
+let newFruits = [...fruits]; /* cara yang benar menggunakan spread operator */
+
+// cara menambahkan value pada last array slot
+fruits.push("Grapes");
+// cara menghapus last value pada array
+fruits.pop(); //menghapus value "Grapes"
+fruits.pop(); //menghapus value "Mango"
+
+console.log(fruits);
+console.log(newFruits);
