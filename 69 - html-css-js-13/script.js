249b19db276559608700f005afd7bd379da2f592
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-08-18 09:46:03 +0700

    Array adalah struktur data di JavaScript yang mirip dengan object, tetapi memiliki index numerik yang ditentukan otomatis oleh JavaScript.
    Index array dimulai dari angka 0 hingga n-1, di mana n adalah jumlah elemen dalam array.
    
    Panjang array disebut length of array, dan bisa diakses dengan properti .length.
    
    Array dapat berisi tipe data primitif (seperti string, number, boolean, null) maupun non-primitif (seperti object, array lain).

---
+++ 69 - html-css-js-13/script.js
@@ -0,0 +1,16 @@
+let fruits = ["Apple", "Banana", "Orange", "Mango"];
+console.log(fruits);
+
+let numbers = [10, 20, 30, 40, 50];
+console.log(numbers);
+
+let mixedArray = [
+  "Hello",
+  26,
+  true,
+  null,
+  { name: "Seno", job: "Software Engineer" },
+  [13, 7, 8],
+];
+
+console.log(mixedArray);
