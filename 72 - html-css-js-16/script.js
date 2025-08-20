c0185f44e9390bdb687780863b8fccf9369ad945
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-08-20 08:27:31 +0700

    Nested for loop bisa digunakan apabila kita memiliki tipe data yang bersarang, misalkan array yang memiliki array di dalamnya
    array =
    [
      [1, 2, 3],
      [4, 5, 6],
      [7, 8, 9],
    ]
    
    ketika kita ingin mengakses value pada array ini, kita misalkan value pada array kita sebut row, sedangkan value yang ada di dalam row itu kita sebut col (dari kta column).
    
    ketika kita mengakses nilai pada value sudah pasti kita akan melakukan:
    array[row]
    dimana kita mengimitasikan row ini sebagai index array tersebut.
    
    lalu pada row tersebut kita ingin mengakses value yang ada di dalamnya. kita imitasikan col sebagai index value pada array row.
    
    maka dihasilkan:
    array[row][col].
    ----------------------------------------------------------------------------------------------
    lalu bagaimana kita mengakses semua nilai pada array ini.
    
    kita bisa menggunakan for loop untuk saat ini.
    
    pertama kita mengakses berulang kali seluruh value pada array.
    kita sudah memisalkan index pada array ini sebagai row.
    
    Jadi:
    for ( let row = 0; row < array.length; row++){}
    
    disini kita sudah dapat mengakses value pada array tersebut.
    
    kita juga dapat mencetak nilai pada array tersebut
    for ( let row = 0; row < array.length; row++){
     console.log(array[row]);
    }
    akan menghasilkan
    [1,2,3],[4,5,6],[7,8,9]
    ---------------------------------------------------------------------------------------------
    Bagaimana kita mengakses nilai 1 sampai 9 di dalam array yang ada pada row
    
    kita lakukan for loop lagi. kita sudah menetapkan col sebagi index untuk mengakses value pada row.
    
    for ( let row = 0; row < array.length; row++){
       for ( let col = 0 ; col < array[row].length; col{
        console.log(array[row][col]);
       }
    }
    ini akan menghasilkan 1,2,3,4,5,6,7,8,9 secara terpisah.

--- 72 - html-css-js-16/script.js
+++ 72 - html-css-js-16/script.js
@@ -1,6 +1,48 @@
-let candies = 10;
-
 // for (var iniliasasi index; kondisi; inkrementasi)
+let candies = 10;
 for (i = 1; i <= candies; i++) {
   console.log(`Sticker is applied to candy #${i}`);
 }
+
+console.log("\n");
+// --------------------------------------------------------
+
+let fruits = ["Apples", "Banana", "Orange", "Mango"];
+// length adalah properti array untuk mengembalikan jumlah elemen yang ada di dalam array
+// fruits.length akan mengembalikan 4
+for (let fruit = 0; fruit <= fruits.length - 1; fruit++) {
+  //  index dimulai dari 0, jadi untuk mengakses elemen terakhir, kita harus mengurangi 1 dari panjang array
+  console.log(fruits[fruit]);
+}
+
+console.log("\n");
+// --------------------------------------------------------
+
+// nested array loop
+let matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+for (let row = 0; row < matrix.length; row++) {
+  // console.log(matrix[row]);
+  for (let col = 0; col < matrix[row].length; col++) {
+    console.log(`Matrix value Row ${row + 1} col ${matrix[row][col]}`);
+  }
+}
+
+let totalSum = 0;
+let numberDescriptions = [];
+console.log("\n");
+for (let i = 1; i <= 10; i++) {
+  console.log(
+    `Total Sum of ${i}, i=${i}: totalSum  + i  = ${totalSum} + ${i} = ${
+      totalSum + i
+    }`
+  );
+  totalSum += i;
+  numberDescriptions.push(`Array[${i - 1}]: ${i}`);
+}
+
+// console.log(numberDescriptions);
