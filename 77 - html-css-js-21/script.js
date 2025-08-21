898d6aae74081e2ef2e6f1f7c9f21954f305f173
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-08-21 10:29:07 +0700

    Ada 4 tipe scope yang dipelajari di file ini.
    1. Global Scope
    scope yang dimana variabel yang dideklarasikan berada di luar code fungsi ataupun code blok (percabangan. pengulangan)
    
    2. Local Scope
    Local scope adalah scope di mana variabel hanya bisa diakses di dalam sebuah fungsi. Variabel dengan local scope tidak bisa diakses dari luar fungsi tersebut.
    
    3. Block Scope
    Block scope adalah scope yang dibatasi oleh sepasang kurung kurawal { }, seperti pada blok if, for, atau while. Variabel yang dibuat dengan let atau const di dalam block scope hanya bisa diakses di dalam blok tersebut.
    
    Catatan:
    
    * Local scope biasanya merujuk pada fungsi, sedangkan block scope berlaku pada blok kode apapun (if, for, while, dsb).
    
    * Variabel di dalam local scope (fungsi) bisa diakses oleh blok di dalam fungsi tersebut, tetapi variabel di dalam block scope hanya bisa diakses di dalam blok itu saja.
    
    4. Nested Scope
    Nested scope merupakan scope yang ada di dalam scope.
    
    Scope yang variabel memiliki dua bagian, scope bagian luar dan scope bagian dalam.
    
    Scope bagian dalam bisa mengakses variabel yang dideklarasikan oleh scope bagian luar.
    
    Namun scope bagian luar tidak bisa mengakses variabel yang dideklarasikan oleh scope bagian dalam.

---
+++ 77 - html-css-js-21/script.js
@@ -0,0 +1,88 @@
+// variabel global adalah variabel yang dapat diakses di seluruh bagian kode, berada di luar fungsi atau blok.
+// scope global adalah ruang lingkup di mana variabel global dapat diakses, yaitu di seluruh kode.
+
+// variabel lokal adalah variabel yang hanya dapat diakses di dalam fungsi atau blok tempat ia dideklarasikan.
+// scope lokal adalah ruang lingkup di mana variabel lokal dapat diakses, yaitu hanya di dalam fungsi atau blok tersebut.
+
+// scope blok adalah ruang lingkup yang berlaku untuk variabel yang dideklarasikan di dalam blok seperti if, for, atau while (percabangan atau pengulangan).
+
+// nested scope adalah konsep di mana fungsi atau blok dapat dideklarasikan di dalam fungsi atau blok lain, sehingga variabel yang dideklarasikan di dalamnya dapat diakses oleh fungsi atau blok luar, tetapi tidak sebaliknya.
+
+var globalVar = "I am a global variable";
+let globalLet = "I am a global let variable";
+
+// global scope
+console.log(`Global Scope Example:`);
+function checkGlobalScope() {
+  console.log(globalVar); // Akses variabel global
+  console.log(globalLet); // Akses variabel global
+}
+
+checkGlobalScope(); //memanggil fungsi checkGlobalScope
+
+console.log(`\n`);
+
+// local scope
+console.log(`Local Scope Example:`);
+
+function localScopeExample() {
+  var localVar = "I am a local variable to this function";
+  let localLet = "I am a local let variable to this function";
+
+  console.log(localVar);
+  console.log(localLet);
+}
+localScopeExample(); //memanggil fungsi localScopeExample
+
+// console.log(localVar);
+// Akan menghasilkan error karena localVar tidak dapat diakses di luar fungsi
+// console.log(localLet);
+// Akan menghasilkan error karena localLet tidak dapat diakses di luar fungsi
+
+console.log(`\n`);
+
+// block scope
+console.log(`Block Scope Example:`);
+
+function blockScopeExample() {
+  if (true) {
+    var blockVar = "I am a block variable";
+    let blockLet = "I am a block let variable";
+
+    console.log(blockVar); // Akses variabel block scope
+    console.log(blockLet); // Akses variabel block scope
+  }
+
+  console.log(`akses di luar scope variabel var: ${blockVar}`); // Akses variabel block scope, karena var dapat diakses di luar blok
+
+  // console.log(blockLet);
+  // Akan menghasilkan error karena blockLet tidak dapat diakss di luar blok percabangan atau pengulangan
+}
+blockScopeExample(); // Memanggil fungsi blockScopeExample
+
+// console.log(blockVar);
+// Akan menghasilkan error karena blockVar tidak dapat diakses di luar fungsi, karena variabel sudah menjadi variabel lokal yang ada di dalam fungsi
+
+console.log(`\n`);
+
+// nested scope
+console.log(`Nested Scope Example:`);
+
+function outerFunction() {
+  var outerVar = "I am an outer variable";
+
+  function innerFunction() {
+    // fungsi innerFunction bisa mengakses variabel outerVar dan innerVar
+
+    var innerVar = "I am an inner variable";
+    console.log(outerVar); // Akses variabel dari fungsi luar
+    console.log(innerVar); // Akses variabel dari fungsi dalam
+  }
+
+  // namun fungsi outerFunction tidak bisa mengakses variabel innerVar, karena innerVar hanya dapat diakses di dalam fungsi innerFunction
+
+  innerFunction(); // Memanggil fungsi dalam
+  // console.log(innerVar);
+  // Akan menghasilkan error karena innerVar tidak dapat diakses di luar fungsi dalam
+}
+outerFunction(); // Memanggil fungsi luar
