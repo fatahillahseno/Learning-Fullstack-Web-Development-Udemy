c7b1cf48c4139f6979573a3e014855816e0ce95d
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-09-15 11:26:27 +0700

    querySelector() dan querySelectorAll()
    Mengizinkan untuk memilih element pada DOM menggunakan CSS Selector
    
    Perbedaan querySelector() dengan querySelectorAll()
    querySelector() akan memilih elemen pertama yang ditemukan
    sedangkan querySelectorAll() akan memilih seluruh elemen yang dicari
    
    Ketika kita menggunakan querySelector dan querySelectorAll
    akan mengembalikan sebuah array NodeList
    NodeList bisa juga diiterasikan ketika diakses value arraynya
    
    Lalu apakah yang membedakan dengan HTML Collection dengan NodeList itu berisikan daftar simpul (list of nodes)
    HTML Collection itu berisikan daftar element (list of elements)

---
+++ 97 - html-css-js-41/script.js
@@ -0,0 +1,15 @@
+let firstHeading = document.querySelector("h1");
+console.log(firstHeading.textContent);
+
+let firstParagraph = document.querySelector(".intro");
+let allParagraphs = document.querySelectorAll(".intro");
+console.log(firstParagraph.textContent);
+console.log(allParagraphs);
+
+let firstListItem = document.querySelector("#list li");
+let allListItems = document.querySelectorAll("#list li");
+console.log(firstListItem.textContent);
+console.log(allListItems);
+
+let allButtons = document.querySelectorAll("button");
+console.log(allButtons);
