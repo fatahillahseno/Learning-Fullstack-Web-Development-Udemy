729006ea17154a58bdda8dcbab16323f1997e3ba
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-09-16 14:42:59 +0700

    childNodes dan children
    sama menghasilkan output berupa
    apa saja child yang ada di dalam suatu parent node
    
    Namnun ada satu perbedaan yang sangat mencolok,yaitu:
    childNodes akan mengembalikan apapun child node
    yang berada di dalam parent node
    baik itu comment, text, dan element
    keluaran tersebut berupa nodeList
    
    sedangkan children hanya akan mengembalikan element saja
    keluaran tersebut berupa HTML collection

---
+++ 103 - html-css-js-47/script.js
@@ -0,0 +1,11 @@
+let parentContainer = document.getElementById("parent");
+let childNodes = parentContainer.childNodes; // nodeList
+let children = parentContainer.children; // HTML Elements
+console.log("childNodes\n", childNodes);
+console.log("children\n", children);
+
+let parentContainer2 = document.getElementById("parent2");
+let childNodes2 = parentContainer2.childNodes;
+let children2 = parentContainer2.children;
+console.log("childNodes2\n", childNodes2);
+console.log("children2\n", children2);
