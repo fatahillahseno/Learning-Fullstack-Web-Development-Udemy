a99b748c82c158a8821ec7ddcdb5b42cd7d7d5b8
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-09-17 14:11:34 +0700

    nodeName dan nodeType
    nodeName => mengetahui nama node apa yang sedang dipilih
    
    nodeType => mengetahui tipe node apa yang sedang dipilih

---
+++ 106 - html-css-js-50/script.js
@@ -0,0 +1,8 @@
+let divElement = document.getElementById("sample-element");
+console.log(divElement);
+console.log(divElement.nodeName);
+console.log(divElement.nodeType);
+
+let textNode = divElement.firstChild;
+console.log(textNode.nodeName);
+console.log(textNode.nodeType);
