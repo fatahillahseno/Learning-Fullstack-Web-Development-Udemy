fc6e6235417b4d8387c3b51d64abd24d96ceaeec
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-09-17 10:13:16 +0700

    firstChild,  firstElementChild, lastChild. lastElementChild
    firstChild => memilih child node pertama pada suatu parent node
    
    firstElementChild => memilih child element  pertama pada suatu parent node
    
    firstChild => memilih child node pertama pada suatu parent node
    
    firstElementChild => memilih child element pertama pada suatu parent node
    
    lastChild => memilih child node terakhir pada suatu parent node
    
    lastElementChild => memilih child element terakhir pada suatu parent node

---
+++ 104 - html-css-js-48/script.js
@@ -0,0 +1,16 @@
+let parent = document.getElementById("parent");
+console.log(parent);
+
+// let childOfParent = parent.childNodes;
+// console.log(childOfParent);
+
+let firstNode = parent.firstChild;
+console.log(firstNode);
+
+let lastNode = parent.lastChild;
+console.log(lastNode);
+
+let firstElement = parent.firstElementChild;
+let lastElement = parent.lastElementChild;
+console.log(firstElement);
+console.log(lastElement);
