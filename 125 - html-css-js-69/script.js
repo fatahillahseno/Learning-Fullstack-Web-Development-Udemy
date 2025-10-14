a930331021a3705e91329a109823cde6c6998122
fatahillahseno <seno.fatahillahsatriabima@gmail.com>
2025-10-14 13:18:08 +0700

    async function dengan try dan catch
    async keyword untuk menandakan function pada  file script.js itu menjadi function khusus (fungsi asinkron). Fungsi tersebut akan selalu mengembalikan promise.
    
    await keyword pada fetch menandakan bahwa fungsi asinkron akan menunggu /berhenti sejenak hingga server dari url memberikan respons yang disimpan pada variabel response
    
    await pada response.json() menandakan bahwa fungsi asinkron akan menunggu lagi hingga isi dari variabel response diubah menjadi json. hasil tersebut akan disimpan ke dalam variabel data
    
    try keyword untuk menandakan mekanisme untuk membungkus kode yang berpotensi gagal/memicu error
    
    catch keyword menandakan mekanisme untuk menangani error yang dilemparkan oleh try keyword
    
    jika try berhasil, maka blok catch dilewati
    
    jika try gagal atau error, maka blok try akan dihentikan lalu blok catch akan dijalankan untuk melakukan penanganan kesalahan

--- 125 - html-css-js-69/script.js
+++ 125 - html-css-js-69/script.js
@@ -1,47 +1,12 @@
-function loadData() {
-  fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then((response) => {
-      if (!response.ok) {
-        //jika response.ok tidak ada
-        throw new Error("Network response was not okay");
-      }
-      // jika response.ok ada
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-    })
-    .catch((error) => {
-      console.group(error);
-    });
+async function loadData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicod.com/posts/1");
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log("Elegantly Handle Error");
+    console.log(error);
+  }
 }
 
 loadData();
-
-// versi yang di atas lebih simple dan ringkas
-
-// let myPromise = new Promise((myResolve, myReject) => {
-//   let req = new XMLHttpRequest();
-//   req.open("GET", "https://jsonplaceholder.typicode.com/posts/1");
-//   req.onload = () => {
-//     if (req.status == 200) {
-//       myResolve(req.response);
-//     } else {
-//       myReject("Unable to fetch data");
-//     }
-//   };
-//   req.send();
-// });
-
-// myPromise.then(
-//   (res) => {
-//     myDisplayer(res);
-//   },
-//   (error) => {
-//     myDisplayer(error);
-//   }
-// );
-
-// function myDisplayer(value) {
-//   console.log(`Printing Result:\n${value}`);
-// }
